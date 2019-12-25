4976a4efd54bccbabb1044038774e83193768332
Hailang <thehailang@gmail.com>
2019-12-26 03:50:11 +0800

    Add `install_bitcoin` script

---
+++ bin/install_bitcoin.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Setup a bitcoin full node on Centos 7
+function install_bitcoin() {
+  # Update dependency
+  yum update -y
+  yum -y install epel-release
+  yum install -y autoconf automake boost-devel gcc-c++ git libdb4-cxx libdb4-cxx-devel libevent-devel libtool openssl-devel wget
+
+  # Clone bitcoin source code
+  git clone https://github.com/bitcoin/bitcoin.git
+  cd bitcoin
+
+  # checkout to a release
+  release_tag="v0.19.0.1"
+  echo checking out "${release_tag}"
+  git checkout "${release_tag}"
+
+  # Build
+  ./autogen.sh
+  ./configure
+  make -j $(nproc)
+}
+
+install_bitcoin
