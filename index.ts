9836961bcc6acba7c39d7c2bc97453f9f999a4a2
Hailang <thehailang@gmail.com>
2020-01-02 17:24:00 +0800

    Auto commit: 2020-01-02_17:24:00

--- index.ts
+++ index.ts
@@ -1,2 +1,4 @@
 export { T_cryptoface } from './src/cryptoface'
+export { T_account } from './src/account'
+export { T_transaction } from './src/transaction'
 
--- src/cryptoface.ts
+++ src/cryptoface.ts
@@ -1,4 +1,7 @@
-export interface T_cryptoface<T_opt,
+import { T_account } from './account'
+import { T_transaction } from './transaction'
+
+export interface T_cryptoface<T_opt, N_tx_currency extends string, N_tx_unit extends string,
   T_IN_create_account, T_IN_get_account,
   T_IN_create_transaction, T_IN_get_transaction> {
 
@@ -6,25 +9,25 @@ export interface T_cryptoface<T_opt,
    * Create one crypto account (could be a "wallet")
    * @param opt
    */
-  create_account(opt?: T_IN_create_account)
+  create_account(opt?: T_IN_create_account): T_account | Promise<T_account>
 
   /**
    * Create many crypto accounts
    * @param opt
    */
-  create_accountS?(optS?: T_IN_create_account[])
+  create_accountS?(optS?: T_IN_create_account[]): T_account[] | Promise<T_account[]>
 
   /**
    * Get one crypto account's (could be a "wallet") info
    * @param opt
    */
-  get_account(opt?: T_IN_get_account)
+  get_account(opt?: T_IN_get_account): T_account | Promise<T_account>
 
   /**
    * Get many crypto accounts' info
    * @param opt
    */
-  get_accountS?(optS?: T_IN_get_account[])
+  get_accountS?(optS?: T_IN_get_account[]): T_account[] | Promise<T_account[]>
 
   /**
    * Create one transaction
@@ -42,11 +45,11 @@ export interface T_cryptoface<T_opt,
    * Get one transaction
    * @param opt
    */
-  get_transaction(opt?: T_IN_get_transaction)
+  get_transaction(opt?: T_IN_get_transaction): T_transaction<N_tx_currency, N_tx_unit> | Promise<T_transaction<N_tx_currency, N_tx_unit>>
 
   /**
    * Get many transactions
    * @param opt
    */
-  get_transactionS?(opt?: T_IN_get_transaction[])
+  get_transactionS?(opt?: T_IN_get_transaction[]): T_transaction<N_tx_currency, N_tx_unit>[] | Promise<T_transaction<N_tx_currency, N_tx_unit>[]>
 }
